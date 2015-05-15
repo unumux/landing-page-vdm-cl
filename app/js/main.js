34783ccf18abff37d35f57bea94c2a64dd47c753
Kyle Wagner <Keale2@gmail.com>
2015-05-15 15:08:54 -0400

    Adds detection of page scroll position.

---
+++ app/js/main.js
@@ -0,0 +1,17 @@
+(function () {
+
+	function getScrollTop() {
+		requestAnimationFrame(getScrollTop);
+		
+		if(document.body.scrollTop > 0) {
+			document.body.classList.add('page-scrolled');
+		}
+		
+		else {
+			document.body.classList.remove('page-scrolled');
+		}
+	}
+	
+	window.addEventListener('DOMContentLoaded', getScrollTop);
+
+})();
